173d777ff3efd77ef69def7ec22728340798db11
AlexTheEpic <99535276+AlexGamer14@users.noreply.github.com>
2024-09-17 13:20:03 +0200

    Update main.cs

--- Core/main.cs
+++ Core/main.cs
@@ -5,7 +5,7 @@ namespace Chess_Engine
     public static class Main_ChessEngine {
         public byte[,] pieces = new byte[8,8];
         public static void Main(string[] args) {
-            
+            Console.WriteLine("Hello World!")
         }
     }
 }
