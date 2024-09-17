dc46aa9e3415b37ec7fc25b62479ec6fa5d9d521
AlexTheEpic <99535276+AlexGamer14@users.noreply.github.com>
2024-09-17 13:29:49 +0200

    hello

--- ChessEngine/Program.cs
+++ ChessEngine/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace ChessEngine {
+    public class ChessEngine {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
