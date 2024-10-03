de62646ae0982d6818678b0875b5692c85642a23
AlexTheEpic <99535276+AlexGamer14@users.noreply.github.com>
2024-10-03 17:33:04 +0200

    Evaluation upd

--- ChessEngine/evalution.cs
+++ ChessEngine/evalution.cs
@@ -1,6 +1,6 @@
 namespace ChessEngine {
     public class Evaluation {
-        public float Evaluate(ChessBoard board) {
+        public float Evaluate(ChessBoard board, bool whiteToMove) {
             float evaluation = 0;
 
             float pawnValue = 100;
@@ -9,7 +9,17 @@ namespace ChessEngine {
             float bishopsValue = 300;
             float queenValue = 900;
 
+            float moveFirstValue = 200;
+
             bool whiteKingAlive = false;
+            bool blackKingAlive = false;
+
+            if (whiteToMove == true) {
+                evaluation+=moveFirstValue;
+            }
+            else {
+                evaluation-=moveFirstValue;
+            }
 
             
             for (int i = 0; i < 64; i++) {
@@ -31,10 +41,32 @@ namespace ChessEngine {
                 if (HelperFunctions.GetByte(i, board.WhiteQueens) == 1) {
                     evaluation += queenValue;
                 }
+
+                if (HelperFunctions.GetByte(i, board.BlackPawns) == 1) {
+                    evaluation -= pawnValue;
+                }
+                if (HelperFunctions.GetByte(i, board.BlackKing) == 1) {
+                    blackKingAlive = true;
+                }
+                if (HelperFunctions.GetByte(i, board.BlackKnights) == 1) {
+                    evaluation -= knightsValue;
+                }
+                if (HelperFunctions.GetByte(i, board.BlackBishops) == 1) {
+                    evaluation -= bishopsValue;
+                }
+                if (HelperFunctions.GetByte(i, board.BlackRooks) == 1) {
+                    evaluation -=  rookValue;
+                }
+                if (HelperFunctions.GetByte(i, board.BlackQueens) == 1) {
+                    evaluation -= queenValue;
+                }
             }
             
             if (!whiteKingAlive)  {
                 evaluation += float.NegativeInfinity;
+            }
+            if (!blackKingAlive) {
+                evaluation+=float.PositiveInfinity;
             }
         
             return evaluation;
