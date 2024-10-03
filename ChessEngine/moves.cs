4f4306c0ae1f1753c85999b8c08629a72da61386
Somast2009 <Sondrems09@gmail.com>
2024-10-03 14:11:27 +0200

    yolo

--- ChessEngine/moves.cs
+++ ChessEngine/moves.cs
@@ -11,8 +11,12 @@ namespace ChessEngine
 
         public bool LegalMove(byte pieceType, byte startPosition, byte endPosition)
         {    
-            // 0 = Pawn, 1 = Knigth, 2 = Bishop, 3 = Rook, 4 = Queen, 5 = King
-            switch (pieceType)
+            // 0 = White Pawn, 1 = White Knigth, 2 = White Bishop, 3 = White Rook, 4 = White Queen, 5 = White King
+            // 6 = Black Pawn, 7 = Black Knigth, 8 = Black Bishop, 9 = Black Rook, 10 = Black Queen, 11 = Black King
+            switch (pieceType) {
+                
+            }
+
 
         }
     }
