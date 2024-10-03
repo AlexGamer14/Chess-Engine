2e10a1d4a4ceba5ee99b8cc6038399c36d9d20af
me3ishere <me3ishere@gmail.com>
2024-10-03 14:16:28 +0200

    Update moves.cs

--- ChessEngine/moves.cs
+++ ChessEngine/moves.cs
@@ -5,7 +5,7 @@ namespace ChessEngine
         public void MovePiece(ChessBoard board, byte startPosition, byte endPosition)
         {
             Debugging.PrintPieces(board.BlackPawns);
-            board.BlackPawns = (ulong)Math.Pow(2, startPosition);
+            board.BlackPawns -= (ulong)Math.Pow(2, startPosition);
             board.BlackPawns += (ulong)Math.Pow(2, endPosition);
         }
 
