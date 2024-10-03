b5a8e1123d2c0bb795be330f9de35407abcd0b7a
Somast2009 <Sondrems09@gmail.com>
2024-10-03 14:06:28 +0200

    Update moves.cs

--- ChessEngine/moves.cs
+++ ChessEngine/moves.cs
@@ -2,11 +2,18 @@ namespace ChessEngine
 {
     public class moves
     {
-        public void MovePiece(ulong pieces, byte startPosition, byte endPosition)
+        public void MovePiece(ChessBoard board, byte startPosition, byte endPosition)
         {
-            Debugging.PrintPieces(pieces);
-            pieces = pieces & ~(ulong)Math.Pow(2, startPosition);
-            pieces = pieces | (ulong)Math.Pow(2, endPosition);
+            Debugging.PrintPieces(board.BlackPawns);
+            board.BlackPawns = (ulong)Math.Pow(2, startPosition);
+            board.BlackPawns += (ulong)Math.Pow(2, endPosition);
+        }
+
+        public bool LegalMove(byte pieceType, byte startPosition, byte endPosition)
+        {    
+            // 0 = Pawn, 1 = Knigth, 2 = Bishop, 3 = Rook, 4 = Queen, 5 = King
+            switch (pieceType)
+
         }
     }
 }
